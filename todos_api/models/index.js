12a4d98e5f86128c7cc96f3c3fefe9f2c2511dfa
YuCheng <henry811010@gmail.com>
2018-09-27 10:40:24 +0000

    Create a todo schema and connect to the model then export it

--- todos_api/models/index.js
+++ todos_api/models/index.js
@@ -3,3 +3,5 @@ var mongoose = require('mongoose');
 mongoose.set('debug', true);
 mongoose.connect('mongodb://localhost/todo-api');
 mongoose.Promise = Promise;
+
+module.exports.Todo = require('/todo');
