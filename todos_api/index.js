259724265913b5e967a9057dbe4f13d067691f46
YuCheng <henry811010@gmail.com>
2018-09-28 10:01:14 +0000

    Create routes and connect to it

--- todos_api/index.js
+++ todos_api/index.js
@@ -2,12 +2,13 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
+var todoRoutes = require('./routes/todos');
+
 app.get('/', function(req, res) {
-    // res.send('Hi there from cloud9');
-    res.json({
-        message: 'Hi there'
-    });
+    res.send('Hello from the root route');
 });
+
+app.use('/api/todos', todoRoutes);
 
 app.listen(port, function() {
     console.log('The app is running on port:' + port);
