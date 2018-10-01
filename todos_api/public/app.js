23be54adf9e8d8e4ae5527bfb8b86a8b94f01440
YuCheng <henry811010@gmail.com>
2018-10-01 08:15:15 +0000

    Add a post request to create a new todo and list it

--- todos_api/public/app.js
+++ todos_api/public/app.js
@@ -2,17 +2,41 @@
 $(document).ready(function() {
     $.getJSON('/api/todos')
     .then(addTodos);
+    
+    $('#todoInput').keypress(function(event) {
+        if(event.which === 13) {
+            createTodo();
+        }
+    });
 });
 
 function addTodos(todos) {
     // Add todos to page here
     todos.forEach(function(todo) {
-        var newTodo = $('<li class="task">' + todo.name + '</li>');
-        
-        if(todo.completed) {
-            newTodo.addClass('done');
-        }
-        
-        $('.list').append(newTodo);
+        addTodo(todo);
+    });
+}
+
+function addTodo(todo) {
+    var newTodo = $('<li class="task">' + todo.name + '</li>');
+    
+    if(todo.completed) {
+        newTodo.addClass('done');
+    }
+    
+    $('.list').append(newTodo);
+}
+
+function createTodo() {
+    // Send request to create new todo
+    var userInput = $('#todoInput').val();
+    
+    $.post('/api/todos', {name: userInput})
+    .then(function(newTodo) {
+        $('#todoInput').val('');
+        addTodo(newTodo);
+    })
+    .catch(function(err) {
+        console.log(err);
     });
 }
