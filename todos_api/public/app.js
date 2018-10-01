45e0d84a31bcc1e4e2f5fbbf1b983d4372796771
YuCheng <henry811010@gmail.com>
2018-10-01 07:55:34 +0000

    Add class done to the list when the todo is completed

--- todos_api/public/app.js
+++ todos_api/public/app.js
@@ -9,6 +9,10 @@ function addTodos(todos) {
     todos.forEach(function(todo) {
         var newTodo = $('<li class="task">' + todo.name + '</li>');
         
+        if(todo.completed) {
+            newTodo.addClass('done');
+        }
+        
         $('.list').append(newTodo);
     });
 }
